65f3b5f86441bfc1869a72e5928f860b5fc30730
Rishav <rishavkumar.nit@gmail.com>
2025-11-03 09:06:14 +0530

    added code for Dynamic Shopping List Application

---
+++ app.js
@@ -0,0 +1,65 @@
+const itemName = document.querySelector("#item-name-input");
+const itemPrice = document.querySelector("#item-price-input");
+const addButton = document.querySelector("#add-item");
+const finalList = document.querySelector(".final-list");
+const grandTotalField = document.querySelector("#Grand-total-price");
+let total = 0;
+
+addButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("here");
+  if (!itemName.value) {
+    alert("please add item name");
+    return;
+  }
+  if (!itemPrice.value) {
+    alert("please add item price");
+    return;
+  }
+  if (isNaN(itemPrice.value)) {
+    alert("price should be an amount");
+    return;
+  }
+
+  const outerDiv = document.createElement("div");
+  outerDiv.classList.add("flex", "flex-row", "gap-2", "mb-2", "item-list");
+  finalList.appendChild(outerDiv);
+
+  const itemNameInput = document.createElement("input");
+  itemNameInput.type = "text";
+  itemNameInput.setAttribute("readonly", "readonly");
+  itemNameInput.classList.add(
+    "border",
+    "w-2/3",
+    "border-gray-300",
+    "rounded-sm",
+    "px-3",
+    "py-1",
+    "focus:outline-none",
+    "bg-gray-200"
+  );
+  itemNameInput.value = itemName.value;
+  outerDiv.appendChild(itemNameInput);
+  itemName.value = "";
+
+  const itemPriceInput = document.createElement("input");
+  itemPriceInput.type = "text";
+  itemPriceInput.setAttribute("readonly", "readonly");
+  itemPriceInput.classList.add(
+    "border",
+    "w-1/3",
+    "border-gray-300",
+    "rounded-sm",
+    "px-3",
+    "py-1",
+    "focus:outline-none",
+    "bg-gray-200",
+    "items-prices"
+  );
+  itemPriceInput.value = itemPrice.value;
+  outerDiv.appendChild(itemPriceInput);
+  itemPrice.value = "";
+
+  total += parseFloat(itemPriceInput.value);
+  grandTotalField.value = total.toFixed(2);
+});
